4ce3674fbdce30188686e87da611bf1935dd15fc
Trevor Plaza <antiooftah@gmail.com>
2013-11-08 12:18:33 -0700

    added javascript code

---
+++ Week 2/expressions_wacky/js/script.js
@@ -0,0 +1,9 @@
+var monsterSize = 16; //16 fluid ounces in one can
+var bigSip = 0.5; // .5 ounces
+var smallSip = 0.1; //.1 ounces
+
+var averageSip = (bigSip+smallSip)/2;
+
+var totalSips = Math.round(monsterSize/averageSip); 
+
+console.log("the total amount of times you can sip on a Monster is " + totalSips + " sips!");
