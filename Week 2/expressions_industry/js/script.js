0cd4ecab43a81e07d342b62ddb4c48650cc29596
Trevor Plaza <antiooftah@gmail.com>
2013-11-08 12:14:41 -0700

    added some notes to the javascript

--- Week 2/expressions_industry/js/script.js
+++ Week 2/expressions_industry/js/script.js
@@ -1,3 +1,10 @@
+/* 
+This is an expression I need to use with my business. 
+I own a granola manufacturer called Olde Man Granola and 
+I needed to figure out how many bags of granola resulted 
+from one cooked batch.
+*/
+
 var batchWeight = 93; // pounds
 var granolaBagWeight = 12; // ounces
 var OzToLb = 16; //16 oz in 1 lb
