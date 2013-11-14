b14f181a231b516a0bd025232d96f3533fe7b4a0
Trevor Plaza <antiooftah@gmail.com>
2013-11-14 15:07:28 -0700

    added comments

--- Week 3/conditionals_personal/js/script.js
+++ Week 3/conditionals_personal/js/script.js
@@ -1,5 +1,5 @@
 //Guitar practice calculator to determine how good you will be
-var guitarExperience = prompt("How many years have you played?");
-var practiceAmount = prompt("How many hours per day do you practice?");
+var guitarExperience = prompt("How many years have you played?"); //user will input their experience
+var practiceAmount = prompt("How many hours per day do you practice?"); //user will input how often they practice
 
-var yourSkill = (guitarExperience>=3)||(practiceAmount>=3) ? console.log("You are going to rock!") : console.log("You need to have more experience!");
+var yourSkill = (guitarExperience>=3)||(practiceAmount>=3) ? console.log("You are going to rock!") : console.log("You need to have more experience!"); //this will print out how if they are on track to be great or if they need to practice more
