ace7972877330bcaa7066c356b469631ffd33096
Trevor Plaza <antiooftah@gmail.com>
2013-11-14 15:13:35 -0700

    corrected an error

--- Week 3/conditionals_business/js/script.js
+++ Week 3/conditionals_business/js/script.js
@@ -6,5 +6,5 @@ var inkCartridgeLeft = 1; //this is how many ink cartridges you have left
 if(inkLevel < .10 || inkCartridgeLeft <= 1){ //this is the condition for the if statement
 	console.log("Go get some more ink quick!"); // this prints out if the condition is true
 }else{ //if the condition is false, the code below gets executed
-	console.log("Your fine for now but you better keep an eye on it."); //if the code is false it prints this
+	console.log("You're fine for now but you better keep an eye on it."); //if the code is false it prints this
 };
