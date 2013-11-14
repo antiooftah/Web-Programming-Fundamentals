83476e1cd233376217a2c01d68a2e673760af83e
Trevor Plaza <antiooftah@gmail.com>
2013-11-14 15:13:02 -0700

    created the statements

--- Week 3/conditionals_business/js/script.js
+++ Week 3/conditionals_business/js/script.js
@@ -2,3 +2,9 @@
 
 var inkLevel = .10; //this is a percentage
 var inkCartridgeLeft = 1; //this is how many ink cartridges you have left
+
+if(inkLevel < .10 || inkCartridgeLeft <= 1){ //this is the condition for the if statement
+	console.log("Go get some more ink quick!"); // this prints out if the condition is true
+}else{ //if the condition is false, the code below gets executed
+	console.log("Your fine for now but you better keep an eye on it."); //if the code is false it prints this
+};
