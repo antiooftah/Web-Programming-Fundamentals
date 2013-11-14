52e75670c0b59c7ec7561caae0a013ce647fc71c
Trevor Plaza <antiooftah@gmail.com>
2013-11-14 15:20:04 -0700

    created variables

--- Week 3/conditionals_wacky/js/script.js
+++ Week 3/conditionals_wacky/js/script.js
@@ -1,1 +1,4 @@
 //This will calculate if you have enough beer for you party!
+
+var beerCurrent = prompt("How much beer do you have in your fridge?"); //This will ask the user how many beers they have
+var guestCount = prompt("How many guests are you expecting?"); //This will ask how many guests are coming
