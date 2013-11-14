a3be041052555566ca5a08f641df648743b49782
Trevor Plaza <antiooftah@gmail.com>
2013-11-14 14:46:46 -0700

    Created the js for the worksheet

---
+++ Week 3/Expressions_worksheet/js/script.js
@@ -0,0 +1,23 @@
+
+//Sparky
+var sparkyDogAge = 7; //This is Sparky's age in dog years
+
+var sparkyHumanAge = sparkyDogAge*7; //This calculates Sparky's human age equivalant
+
+console.log("Sparky is " +sparkyHumanAge + " human years old which is " + sparkyDogAge + " years old in dog years"); //This outputs the comment.
+
+//Pie Part I
+var slices = 8; //slices per pizza
+var people = 27; //number of people eating
+var pizza = 6; //number of pizzas
+
+var slicesPer = pizza*slices/people; //slices allowed per person
+var slicesPerModulus = slicesPer%2; //slices allowed per person modulus
+
+console.log("Each person ate " + slicesPer+ " slices of pizza at the party."); //printed string
+console.log("Each person ate " + slicesPerModulus+ " slices of pizza at the party."); //printed string modulus
+
+
+
+
+// Pie Part II
