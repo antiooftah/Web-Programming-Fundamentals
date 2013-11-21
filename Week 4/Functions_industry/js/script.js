2a7c45f7bbe1caa20f3702331a8be2f4ce177234
Trevor Plaza <antiooftah@gmail.com>
2013-11-21 12:28:12 -0700

    created variables

--- Week 4/Functions_industry/js/script.js
+++ Week 4/Functions_industry/js/script.js
@@ -1,1 +1,5 @@
 //Calculator to calculate the margins and markup for retail and distribution in the retail environment
+
+var retail = prompt("How much are you selling your product to stores for?"); //Asks user what the price to a store would cost
+var distributor = prompt("How much are you selling your product to a distributor for?"); //Asks the user what the price is for a distributor
+var marginPercent = prompt("what is the average markup percentage in decimal format for your type of product?"); //asks what percentage of a markup the client requires
