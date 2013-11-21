b5b1b110c2fe6f7ef054b04d321d34ed87b639c4
Trevor Plaza <antiooftah@gmail.com>
2013-11-21 12:18:37 -0700

    created the variables

--- Week 4/Functions_personal/js/script.js
+++ Week 4/Functions_personal/js/script.js
@@ -1,1 +1,7 @@
 // Calculator to show how much it will cost to buy the whole series of a show vs each episode by itself.
+
+//var whatShow = prompt("What show do you want to buy?");
+var singleShow = prompt("How much does each episode cost?"); //asks the user how much each episode costs
+var fullSeason = prompt("how much does each season cost?"); //asks the user how much each season costs
+var episodeEachSeason = prompt("how many episodes are there in each season?"); //asks the user how many episodes there are in each season
+var seasonEachSeries= prompt("How many seasons are there?"); //asks the user how many seasons there are
