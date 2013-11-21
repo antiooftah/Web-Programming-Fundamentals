baddef719e40e0c8407a703af298cf8875538bcd
Trevor Plaza <antiooftah@gmail.com>
2013-11-21 12:21:39 -0700

    created the function

--- Week 4/Functions_personal/js/script.js
+++ Week 4/Functions_personal/js/script.js
@@ -5,3 +5,9 @@ var singleShow = prompt("How much does each episode cost?"); //asks the user how
 var fullSeason = prompt("how much does each season cost?"); //asks the user how much each season costs
 var episodeEachSeason = prompt("how many episodes are there in each season?"); //asks the user how many episodes there are in each season
 var seasonEachSeries= prompt("How many seasons are there?"); //asks the user how many seasons there are
+
+function showCalc(episodes,seasons,episodeCost,seasonCost){ //creates a function and the perameters for the function
+	var eachTotal = seasonCost*seasons; //creates a variable and calculates the total price for the seasons all together
+	var eachEpisode = seasons*episodes*episodeCost; //creates a variable and calculates the total price for the episodes all together
+	console.log("It will cost you $" + eachEpisode + " to buy each episode individually or $" + eachTotal + " to buy the season."); //this prints out the totals
+}
