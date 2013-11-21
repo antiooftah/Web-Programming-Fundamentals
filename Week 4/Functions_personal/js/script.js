147dc486e035d3f6f63bd4d862d8af05dcb0ee6a
Trevor Plaza <antiooftah@gmail.com>
2013-11-21 12:22:14 -0700

    deleted an unused line

--- Week 4/Functions_personal/js/script.js
+++ Week 4/Functions_personal/js/script.js
@@ -1,6 +1,5 @@
 // Calculator to show how much it will cost to buy the whole series of a show vs each episode by itself.
 
-//var whatShow = prompt("What show do you want to buy?");
 var singleShow = prompt("How much does each episode cost?"); //asks the user how much each episode costs
 var fullSeason = prompt("how much does each season cost?"); //asks the user how much each season costs
 var episodeEachSeason = prompt("how many episodes are there in each season?"); //asks the user how many episodes there are in each season
