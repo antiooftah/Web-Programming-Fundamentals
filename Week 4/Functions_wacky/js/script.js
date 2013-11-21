c2e3b8ce342b8c691374dc09eefd5355f28c23ba
Trevor Plaza <antiooftah@gmail.com>
2013-11-21 12:36:09 -0700

    created variables

--- Week 4/Functions_wacky/js/script.js
+++ Week 4/Functions_wacky/js/script.js
@@ -1,1 +1,4 @@
 //Calculates and tells you if you are watching too much TV
+
+var energyBill = prompt("how much is your energy bill this month?"); //asks the user the energy bill cost
+var cableBill = prompt("How much did you pay for TV this month?"); //asks the user the cable cost
